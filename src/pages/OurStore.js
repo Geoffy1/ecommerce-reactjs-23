47c872a2ed4537fa4ab85fac7e092cd18b9cce62
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2024-05-30 14:20:17 +0300

    added grid lines in Product component

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BreadCrumb from '../components/BreadCrumb'
 import ReactStars from "react-rating-stars-component";
 import Meta from '../components/Meta'
-
+import ProductCard from "../components/ProductCard"
 
 const OurStore = () => {
+  const [grid,setGrid] = useState(4);
+  alert(grid);
+  
 
   return (
     <>
@@ -18,12 +21,15 @@ const OurStore = () => {
                 <h3 className="filter-title">
                   Shop by Categories
                 </h3>
-                <ul>
+                <div>
+                  <ul>
                   <li>Watch</li>
                   <li>laptop</li>
                   <li>TV</li>
                   <li>camera</li>
                 </ul>
+                </div>
+                
               </div>
               <div className="filter-card mb-3">
                 <h3 className="filter-title">
@@ -140,9 +146,40 @@ const OurStore = () => {
                 </div>
               </div>
             </div>
+            <div className="col-9">
+            <div className='filter=-sort-grid mb-4'>
+              <div className='d-flex justify-content-between align-items-center'>
+                <div className= " d-flex align-items-center gap-10">
+                <p className='mb-0 d-block'>Sort By:</p>
+                <select name="" id="" className='form-control form-select'>
+                  <option value="manual">Featured</option>
+                  <option value="best-selling">Best Selling</option>
+                  <option value="title-ascending">Alphabetically, A-Z</option>
+                  <option value="title-descending">Alphabetically, Z-A</option>
+                  <option value="price-ascending">Price, low to high</option>
+                  <option value="price-descending">Price, high to low</option>
+                  <option value="created-ascending">Date, old to new</option>
+                  <option value="created-descending">Date, new to old</option>
+                </select>
+              </div>
+              <div className="d-flex align-items-center gap-10">
+                <p className="totalproducts mb-0 " style={{"width":"100px"}}>21 products</p>
+                <div className="d-flex align-items-center gap-10 grid">
+                  <img onClick={()=>{setGrid(4);}} src="images/gr4.svg" alt="grid" className="d-block img-fluid" />
+                  <img onClick={()=>{setGrid(3);}} src="images/gr3.svg" alt="grid" className="d-block img-fluid" />
+                  <img onClick={()=>{setGrid(2);}} src="images/gr2.svg" alt="grid" className="d-block img-fluid" />
+                  <img onClick={()=>{setGrid(1);}} src="images/gr.svg" alt="grid" className="d-block img-fluid" />
+                </div>
+              </div>
+              </div>
+            </div>
+            <div className="products-list pb-5">
+              <ProductCard grid={grid}/>
+            </div>
+          </div>
           </div>
 
-          <div className="col-9"></div>
+          
         </div>
       </div>
     </>
