3537e24e49295fc4fa854b8c2a1f498255f01ae7
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2024-05-30 21:51:12 +0300

    finished my store page and styled it

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,9 +5,54 @@ import ReactStars from "react-rating-stars-component";
 const ProductCard = (props) => {
   const {grid} = props;
   let location = useLocation();
-  alert(location);
+  // alert(location);
   return (
-    <div className={`${location.pathname == "/store" ? `col-${grid}` : "col-3"} `}>
+    <>
+      <div className={`${location.pathname == "/store" ? `gr-${grid}` : "col-3"} `}>
+      <div className="product-card position-relative">
+        <div className="wishlist-icon position-absolute">
+            <Link>
+              <img src="images/wish.svg" alt="wishlist" />
+            </Link>
+        </div>
+        <div className="product-image">
+          <img src="images/watch.jpg" className='img-fluid' alt="product" />
+          <img src="images/watch-1.avif" className='img-fluid' alt="product" />
+        </div>
+        <div className="product-details">
+          <h5 className="product-title">
+            Kids headphones wholesale
+          </h5>
+          <ReactStars
+    count={5}
+    size={24}
+    edit={false}
+    value='3'
+    activeColor="#ffd700"
+  />
+  <p className={`'description' ${grid=== 12 ? "d-block" : "d-none"}`} >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo culpa, aliquid?</p>
+ 
+ 
+          <p className="price">$100.00</p>
+          
+        </div>
+        <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+            <Link>
+                  <img src="images/prodcompare.svg" alt="compare" />
+                </Link>
+                <Link>
+                  <img src="images/add-cart.svg" alt="addcart" />
+                </Link>
+                <Link>
+                  <img src="images/view.svg" alt="view" />
+                </Link>
+                
+            </div>
+        </div>
+      </div>
+    </div>
+    <div className={`${location.pathname == "/store" ? `gr-${grid}` : "col-3"} `}>
       <div className="product-card position-relative">
         <div className="wishlist-icon position-absolute">
             <Link>
@@ -30,6 +75,7 @@ const ProductCard = (props) => {
     value='3'
     activeColor="#ffd700"
   />
+  <p className={`'description' ${grid=== 12 ? "d-block" : "d-none"}`} >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo culpa, aliquid?</p>
  
           <p className="price">$100.00</p>
           
@@ -50,6 +96,7 @@ const ProductCard = (props) => {
         </div>
       </div>
     </div>
+    </>
   )
 }
 
