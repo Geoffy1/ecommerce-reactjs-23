b5afd8ca72b1d066824d1c6d582ddcf1084ef42d
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2024-06-07 11:54:16 +0300

    Update TermsAndConditions.js removed unused imports

--- src/pages/TermsAndConditions.js
+++ src/pages/TermsAndConditions.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Meta from '../components/Meta'
 import BreadCrumb from '../components/BreadCrumb'
-import {Link} from 'react-router-dom'
+
 
 const TermsAndConditions = () => {
   return (
