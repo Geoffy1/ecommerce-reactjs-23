c1d7003f65377966d9f8df8991165f6948d84eb8
gooffyI <123340848+gooffyI@users.noreply.github.com>
2024-06-11 10:50:01 +0300

    added cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -46,32 +46,6 @@ const Cart = () => {
                             <h5 className="price">$ 100</h5>
                         </div>
                     </div>
-                    <div className="cart-data py-3 mb-2 d-flex justify-content-between align-items-center">
-                        <div className="cart-col-1 d-flex align-items-center">
-                            <div className="w-25">
-                                <img src={watch} alt="product image" />
-                            </div>
-                            <div className="w-75">
-                                <p>title</p>
-                                <p>S</p>
-                                <p>#A5</p>
-                            </div>
-                        </div>
-                        <div className="cart-col-2">
-                            <h5 className="price">$ 100</h5>
-                        </div>
-                        <div className="cart-col-3 d-flex align-items-center gap-15">
-                            <div>
-                                <input className='form-control' min={1} max={140} type="number" id='' />
-                            </div>
-                            <div>
-                                <RiDeleteBinFill className='text-danger' />
-                            </div>
-                        </div>
-                        <div className="cart-col-4">
-                            <h5 className="price">$ 100</h5>
-                        </div>
-                    </div>
                 </div>
                 <div className="col-12 py-2">
                     <div className="d-flex justify-content-between align-items-baseline">
---
+++ src/pages/Checkout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Meta from '../components/Meta'
+import BreadCrumb from '../components/BreadCrumb'
+import { Link } from 'react-router-dom';
+
+
+const Checkout = () => {
+  return (
+    <>
+      <Meta title={"Login"} />
+      <BreadCrumb title='Login' />
+    </>
+  )
+}
+export default Checkout
