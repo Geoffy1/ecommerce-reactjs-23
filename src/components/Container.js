5379b92027ff8696ee631ffcf44fe4cc997b70a7
gooffyI <123340848+gooffyI@users.noreply.github.com>
2024-06-16 13:52:56 +0300

    imported container component into home

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,15 +1,15 @@
 import React from 'react'
 // import { Link, useLocation } from 'react-router-dom'
 
-const Container = () => {
+const Container = (props) => {
   return (
-    <>
-      <section>
+    
+      <section className='props.class1'>
         <div className="container-xxl">
             {props.children}
         </div>
       </section>
-    </>
+    
   )
 }
 export default Container
